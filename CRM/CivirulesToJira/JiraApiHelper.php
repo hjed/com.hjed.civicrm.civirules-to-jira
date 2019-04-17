c959f5eaeb529a6b3407ed02fcdecad35b5830fe
hjed <harry@dayfamilyweb.com>
2019-04-17 22:33:51 +1000

    Add support for basic auth tokens to call user api

--- CRM/CivirulesToJira/JiraApiHelper.php
+++ CRM/CivirulesToJira/JiraApiHelper.php
@@ -95,7 +95,6 @@ class CRM_CivirulesToJira_JiraApiHelper {
   public static function retrieveJiraCloudId() {
 
     $ch = curl_init( 'https://api.atlassian.com/oauth/token/accessible-resources');
-//    $ch = curl_init( 'http://localhost:1500');
     curl_setopt_array($ch, array(
       CURLOPT_RETURNTRANSFER => TRUE,
     ));
@@ -164,6 +163,59 @@ class CRM_CivirulesToJira_JiraApiHelper {
   }
 
   /**
+   * Call a JIRA api endpoint with an API token rather than our normal oauth method
+   * used for operations that require more access.
+   *
+   * @param string $path the path after the jira base url
+   *  Ex. /rest/api/3/groups/picker
+   * @param string $method the http method to use
+   * @param array $body the body of the post request
+   * @return array | CRM_Core_Error
+   */
+  public static function callJiraApiWithToken($path, $method = "GET", $body = NULL) {
+
+    // build the url
+    $url = Civi::settings()->get("jira_api_token_site") .
+      '/' .
+      $path;
+
+    $ch = curl_init($url);
+    curl_setopt_array($ch, array(
+      CURLOPT_RETURNTRANSFER => TRUE,
+      CURLOPT_CUSTOMREQUEST => $method
+    ));
+    if($body != NULL) {
+      $encodedBody = json_encode($body);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedBody);
+    }
+    curl_setopt(
+      $ch,
+      CURLOPT_HTTPHEADER,
+      array(
+        'Accept: application/json',
+        'Content-Type: application/json'
+      )
+    );
+    curl_setopt(
+      $ch,
+      CURLOPT_USERPWD,
+      Civi::settings()->get('jira_api_token_email') . ':' . Civi::settings()->get('jira_api_token')
+    );
+
+    $response = curl_exec($ch);
+    if (curl_errno($ch) || curl_getinfo($ch, CURLINFO_HTTP_CODE) >= 300) {
+      print 'Request Error:' . curl_error($ch);
+      print '<br/>\nStatus Code: ' . curl_getinfo($ch, CURLINFO_HTTP_CODE);
+      print_r($response);
+      throw new CRM_Extension_Exception("JIRA API Request Failed");
+      return CRM_Core_Error::createError("Failed to access jira API");
+      // TODO: handle this better
+    } else {
+      return json_decode($response, true);
+    }
+  }
+
+  /**
    * Retrieve the id of the custom field "jira_user_key"
    * @return int|null|string
    * @throws CiviCRM_API3_Exception
@@ -181,7 +233,7 @@ class CRM_CivirulesToJira_JiraApiHelper {
   public static function createJiraUser(&$contactId) {
     $contactDetails = CRM_Contact_BAO_Contact::getContactDetails($contactId);
 
-    $response = self::callJiraApi(
+    $response = self::callJiraApiWithToken(
       '/rest/api/3/user', "POST", array(
         "emailAddress" => $contactDetails[1],
         "displayName" => $contactDetails[0],
